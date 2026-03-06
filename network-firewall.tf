2fb4047d77670fe3ccb9fcdc60af3fc42ecd483e
palomadiez <202204513@alu.comillas.edu>
2026-03-06 11:20:12 +0100

    firewall rules

--- network-firewall.tf
+++ network-firewall.tf
@@ -1,22 +1,46 @@
+# create network
+resource "google_compute_network" "vpc_network" { 
+  name = var.gcp_network 
+} 
+
 # allow http traffic
-resource "google_compute_firewall" "allow-http" {
-  name    = "tf-fw-allow-http"
-  network = var.gcp-network
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "terraform_allow_icmp"
+  network = google_compute_network.vpc_network.name
   allow {
-    protocol = "tcp"
-    ports    = ["80"]
+    protocol = "icmp"
   }
-  target_tags   = ["http"]
   source_ranges = ["0.0.0.0/0"]
 }
+
 # allow ssh traffic
-resource "google_compute_firewall" "allow-ssh" {
-  name    = "tf-fw-allow-ssh"
-  network = var.gcp-network
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "terradorm_allow_ssh"
+  network = google_compute_network.vpc_network.name 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
-  target_tags   = ["http"]
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "firewall_internal" {
+  name    = "terraform_allow_internal" 
+  network = google_compute_network.vpc_network.name
+
+allow { 
+    protocol = "tcp" 
+    ports    = ["0-65535"] 
+  } 
+ 
+  allow { 
+    protocol = "udp" 
+    ports    = ["0-65535"] 
+  } 
+ 
+  allow { 
+    protocol = "icmp" 
+  } 
+ 
+  source_ranges = ["10.128.0.0/20"] 
 }
